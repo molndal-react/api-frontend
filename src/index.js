c499f196c9f631672d71c1f892f5c34bf7aed711
Michael Dragomir <michael@weapp.se>
2020-06-17 11:06:10 +0200

    Add fetch request

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/customers")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
+  const handleLogin = () => {
+    if (email && password) {
+      fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+  };
+  return (
+    <>
+      <p>Email</p>
+      <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
+      <p>Password</p>
+      <input
+        value={password}
+        type={"password"}
+        onChange={(e) => setPassword(e.target.value)}
+      ></input>
+
+      <button onClick={handleLogin}>Login</button>
+    </>
+  );
+};
+
+ReactDOM.render(<App count={0} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
